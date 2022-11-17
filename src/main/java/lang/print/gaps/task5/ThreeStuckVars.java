120ea4d14b37deab9fb1e941705c679e7501c5f0
Husanboy <olimboyevhusanboy@gmail.com>
2022-11-17 11:42:52 +0500

    yes

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task5;
-
+import java.util.Scanner;
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int first=1;
+        int second=10;
+        int third=100;
+        System.out.println(first +""+ second +""+ third);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task5;
-
+import java.util.Scanner;
 public class TimeConvertor {
     public void convert(float minutes) {
+        Scanner sc=new Scanner(System.in);
+        int a;
+        a=sc.nextInt();
+        float min=a*60;
+        System.out.println(min);
+
     }
 }
